2db1014df3fbab7c433a7a7cd66f418aca3bb2c8
torchg2 <giantrch@gmail.com>
2026-09-21 16:16:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V="ricomp-v3";
+const V="ricomp-v3.2";
 const FILES=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(
